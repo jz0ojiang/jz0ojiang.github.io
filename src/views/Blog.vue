6a00ef321078d1ea57fd0dd387fa0ef41ea996fa
Jz0ojiang <jz0ojiang@qq.com>
2024-03-20 12:43:19 +0800

    UPDATE: 简化代码，支持cn/top分流

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -14,13 +14,11 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 const regex = new RegExp('\\/blog(\\/.*)', 'gm');
 const match = regex.exec(route.fullPath);
-if (match) {
-    window.location.href = `https://blog.im0o.top${match[1]}`;
-    // window.location.href = `https://blog.im0o.cn${match[1]}`;
-} else {
-    window.location.href = 'https://blog.im0o.top';
-    // window.location.href = 'https://blog.im0o.cn';
-}
+const isCn = window.location.href.includes('im0o.cn');
+const baseDomain = isCn ? 'https://blog.im0o.cn' : 'https://blog.im0o.top';
+const redirectPath = match ? match[1] : '';
+
+window.location.href = `${baseDomain}${redirectPath}`;
 </script>
 
 <style lang="scss" scoped>
